6a4ceea2abade5ca6a6a6a3a3dcf8a5ca3ec8df1
KyawMoeOo_KMO <kmo295673@gmail.com>
2026-03-06 00:46:38 +0630

    Initial commit - .NET game project

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,14 @@
+using GameStore.Api.Data;
+using GameStore.Api.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connString = "Data Source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
+var app = builder.Build();
+
+app.MapGamesEndpoints();
+// app.MapGet("/", () => "Hello World!");
+
+app.Run();
